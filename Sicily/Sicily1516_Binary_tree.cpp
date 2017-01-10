c47cdb9d7a20d9fec3da334eec17a4aa8acb81ac
Squarepants <chenhy98@mail2.sysu.edu.cn>
2017-01-10 14:13:13 +0800

    Update Sicily1516_Binary_tree.cpp

--- Sicily/Sicily1516_Binary_tree.cpp
+++ Sicily/Sicily1516_Binary_tree.cpp
@@ -5,11 +5,11 @@ struct tree_node{
 	tree_node *left;
 	tree_node *right;
 	char ch;
-	bool is_root;
-	tree_node():left(NULL),right(NULL),ch('^'),is_root(1){}
+	bool is_root;        //根节点标致，初始化为1                      	
+        tree_node():left(NULL),right(NULL),ch('^'),is_root(1){}
 };
 
-void pre_order(tree_node *T)
+void pre_order(tree_node *T)  //递归 二叉树前序遍历
 {
 	if(!T)
 	return;
@@ -23,7 +23,7 @@ int main()
 	int n;
 	while(cin >> n && n!=EOF)
 	{
-		tree_node *_list[1001];
+		tree_node *_list[1001];  
 		_list[0]=NULL;
 		for(int i=1;i<1001;i++)
 		{
@@ -31,24 +31,24 @@ int main()
 		}
 		int num,l,r;
 		char c;
-		vector<int> root;
+		vector<int> root;  //存出现过的节点标号num                                     		                                
 		for(int i=0;i<n;i++)
 		{
 			cin >> num >> c >> l >> r;
-			root.push_back(num);
+			root.push_back(num); //存入			
 			_list[num]->ch=c;
 			_list[num]->left=_list[l];
 			_list[num]->right=_list[r];
 			if(l!=0)
-			_list[l]->is_root=0;
+			_list[l]->is_root=0;   //作为子树的结点显然不可能是根节点                    
 			if(r!=0)
-			_list[r]->is_root=0;
+			_list[r]->is_root=0;   //作为子树的结点显然不可能是根节点  
 		
 		}
 		int root_num;
 		for(int i=0;i<root.size();i++)
 		{
-			if(_list[root[i]]->is_root==1)
+			if(_list[root[i]]->is_root==1)  //根据is_root(bool）判断并寻找根节点                       
 			root_num=root[i];
 		}
 		tree_node *_root=_list[root_num];
